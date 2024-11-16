8dc844612bf46eac16b00122214928b478b08344
mahmoudalaam1235 <mahmoudalaam510@gmail.com>
2024-11-16 23:40:56 +0200

    make some edite in search text field

--- lib/features/search/ui/search_screen.dart
+++ lib/features/search/ui/search_screen.dart
@@ -1,6 +1,11 @@
 import 'package:book_app/core/theming/colors_app.dart';
+import 'package:book_app/core/theming/text_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/routing/route_name.dart';
+import '../../home/wigets/item_best_seller.dart';
+import '../widgets/custom_search_text_filed.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -10,85 +15,47 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  bool focus = false;
-  bool textEmpty = true;
-  TextEditingController searchController=TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorsApp.darkBlue,
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
+        child: Padding(
+          padding: EdgeInsets.only(right: 25.w,),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                textEmpty
-                    ? IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(
-                          Icons.arrow_back,
-                          color: Colors.white,
-                        ))
-                    : IconButton(
-                        onPressed: () {
-                          focus = false;
-                          textEmpty=true;
-                          searchController.clear();
+              CustomSearchTextFiled(),
+          SizedBox(height: 20.h,),
+          Padding(
+            padding: EdgeInsets.only(left: 25.w),
 
-                          setState(() {});
-                        },
-                        icon: const Icon(
-                          Icons.clear,
-                          color: Colors.white,
-                        )),
-                Container(
-                  width: 300.w,
-                  child: TextField(
-                    controller: searchController,
-                    cursorRadius: const Radius.circular(10),
-                    canRequestFocus: true,
-                    cursorColor: Colors.white,
-                    onChanged:(value) {
-                      setState(() {
-                        value.isEmpty?textEmpty=true:textEmpty=false;
-
-
-                       setState(() {
-
-                       });
-                      });
-                    },
-                    decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                          onPressed: () {},
-                          icon: focus == true
-                              ? const Icon(
-                                  Icons.search_sharp,
-                                  color: Colors.white,
-                                )
-                              : const Icon(
-                                  Icons.search_sharp,
-                                  color: ColorsApp.witheGray,
-                                )),
-                      hintText: "search",
-                      enabledBorder: OutlineInputBorder(
-                          borderSide:
-                              const BorderSide(color: ColorsApp.witheGray),
-                          borderRadius: BorderRadius.circular(15)),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.white),
-                          borderRadius: BorderRadius.circular(15)),
-                    ),
-                  ),
-                )
-              ],
-            )
+            child: Text(
+              "Search Result",
+              style: TextApp.font18WithSemiBold,
+            ),
+          ),
+          Expanded(
+              child: Padding(
+                padding:  EdgeInsets.only(left: 25.w),
+                child: ListView.builder(
+                  padding: EdgeInsets.zero,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (context, index) =>
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.h),
+                        child: GestureDetector(
+                            onTap: () =>
+                                Navigator.pushNamed(
+                                    context, RouteNames.detailsBookScreen),
+                            child: const ItemBestSeller()),
+                      ),
+                  itemCount: 10,
+                ),
+              ))
           ],
         ),
       ),
-    );
+    ),);
   }
 }
